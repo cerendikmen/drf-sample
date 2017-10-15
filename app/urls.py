006cdb45a3b82639faa9b3dd0fe39918dcd78db6
cerendikmen <e2043545@ceng.metu.edu.tr>
2017-10-15 18:36:20 +0300

    django-extensions installed and urlpatterns for viewsets generated

--- app/urls.py
+++ app/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from retail.views import ChainViewSet, StoreViewSet, EmployeeViewSet
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+router = DefaultRouter()
+router.register(prefix='chains', viewset=ChainViewSet)
+router.register(prefix='stores', viewset=StoreViewSet)
+router.register(prefix='employees', viewset=EmployeeViewSet)
+
+urlpatterns = router.urls
